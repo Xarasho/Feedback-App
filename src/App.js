661efc76ff0fa4cc133de2f843ecc57224b0bad4
Alex Rojas <alex.olmedo.rojas@gmail.com>
2023-03-04 16:59:31 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
 export default App;
+
+// const title = 'Blog Post';
+// const body = 'This is my blog post';
+// const comments = [
+//   { id: '1', text: 'Comment 1' },
+//   { id: '2', text: 'Comment 2' },
+//   { id: '3', text: 'Comment 3' },
+// ];
+
+// const loading = false;
+// const showComments = true;
+
+// if (loading) return <h1>Loading...</h1>;
+
+// const commentBlock = (
+//   <div className="comments">
+//     <h3>Comments ({comments.length})</h3>
+//     <ul>
+//       {comments.map((comment, index) => (
+//         <li key={index}>{comment.text}</li>
+//       ))}
+//     </ul>
+//   </div>
+// );
+
+// return (
+//   <div className="container">
+//     <h1>{title.toUpperCase()}</h1>
+//     <p>{body}</p>
+
+//     {showComments && commentBlock}
+//   </div>
+// );
